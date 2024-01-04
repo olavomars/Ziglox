41094f6e7ad5a5fea404a3c9527b02a4f6975123
Olavo Mars <olavoamneves@gmail.com>
2024-01-03 21:25:50 -0400

    first commit

---
+++ src/main.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    // Parse args into string array (error union needs 'try')
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    // Get and print them!
+    std.debug.print("There are {d} args:\n", .{args.len});
+    for (args) |arg| {
+        std.debug.print("  {s}\n", .{arg});
+    }
+}
